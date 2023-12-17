fd648830231ad3eefb667c0f139ccad921c4ba44
bizbaeja <bjgbjg112@gmail.com>
2023-12-17 12:35:32 +0900

    dark theme

--- twitch/app/(auth)/_components/logo.tsx
+++ twitch/app/(auth)/_components/logo.tsx
@@ -9,8 +9,25 @@ const font = Poppins({
 
 export const Logo = () =>{
     return (
-        <div>
-            Logo!
+        <div className='flex flex-col items-center gap-y-4'>
+            <div className='bg-white rounded-full p-1'>
+                <Image
+                 src='/spooky.svg'
+                 width={80} 
+                 height={80}
+                alt='Gaemehub'/>
+            </div>
+            <div className='flex flex-col items-center'>
+                <p className={cn(
+                    "text-xl font-semibold",
+                    font.className
+                )}>
+                     Gamehub
+                </p>
+                <p className={cn("text-sm text-muted-foreground",font.className)}>
+                    Let&apos;s Play
+                </p>
+            </div>
         </div>
     )
 }
--- twitch/app/(auth)/layout.tsx
+++ twitch/app/(auth)/layout.tsx
@@ -5,10 +5,15 @@ import { Logo } from "./_components/logo";
         children: React.ReactNode
     }) => {
         return(
-            <div className="h-full flex items-center justify-center">
+            <main className="h-full flex flex-col items-center justify-center space-y-6">
+                <div>
                 <Logo />
-                {children}
-            </div>
+                </div>
+              <div>
+                     {children}
+              </div>
+           
+            </main>
         )
     }
 
