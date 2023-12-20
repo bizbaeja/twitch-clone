f49606ae91ad3403454a57cfd6615fadd9a2d502
bizbaeja <bjgbjg112@gmail.com>
2023-12-20 20:30:58 +0900

    navbar logo

---
+++ twitch/app/(browse)/layout.tsx
@@ -0,0 +1,18 @@
+import { NavBar } from "./_components/navbar";
+
+const BrowseLayout = ({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) => {
+    return ( 
+      <>
+      <NavBar />
+      <div className="flex h-full pt-20">
+      {children}
+      </div>
+      </>
+    );
+  };
+   
+  export default BrowseLayout;
