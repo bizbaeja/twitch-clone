5cbbd09ec1c6ed0e58701487e8cd845cc37011d0
bizbaeja <bjgbjg112@gmail.com>
2023-12-24 14:26:07 +0900

    hydration error

--- twitch/app/(browse)/_components/sidebar/wrapper.tsx
+++ twitch/app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,18 +1,33 @@
 "use client"
 import { useSidebar } from "@/store/use-sidebar"
+import { useIsClient } from "usehooks-ts"
 import { cn } from "@/lib/utils"
+import { useEffect, useState } from "react"
+import { Toggle, ToggleSkeleton } from "./toggle"
+import { RecommendedSkeleton } from "./user-item"
+
 interface WrapperProps {
     children: React.ReactNode
 };
 
 export const Wrapper = ({ children }: WrapperProps) => {
+    const isClient = useIsClient();
     const { collapsed } = useSidebar((state)=> state);
+
+   
+    if(!isClient) {return (
+        <aside className="fixed left-0 flex  flex-col  w-60  h-full bg-background border-r border-[#2D2E35] z-50">
+            <ToggleSkeleton />
+            <RecommendedSkeleton />
+        </aside>
+    
+    );}
 
     return (
        <aside
         className={cn(
             
-            "fixed left-0 flex  flex-col w-60  h-full bg-background border-r border-[#2D2E35] z-50"
+            "fixed left-0 flex  flex-col  w-[70px] lg:w-60  h-full bg-background border-r border-[#2D2E35] z-50"
             ,
             collapsed && "w-[70px]"
             )}
