c02e8514bf3f602e103a55f4c74b8c771f4984ba
bizbaeja <bjgbjg112@gmail.com>
2024-01-05 16:38:23 +0900

    deploy4

--- twitch/app/api/webhooks/livekit/route.ts
+++ twitch/app/api/webhooks/livekit/route.ts
@@ -1,6 +1,6 @@
 import { headers } from "next/headers";
 import { WebhookReceiver } from "livekit-server-sdk";
-import { NextApiRequest, NextApiResponse } from 'next';
+
 import { db } from "@/lib/db";
 
 const receiver = new WebhookReceiver(
@@ -39,46 +39,5 @@ export async function POST(req: Request) {
         isLive: false,
       },
     });
-  }
-}
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-  const body = await req.body();
-  const headerPayload = headers();
-  const authorization = headerPayload.get("Authorization");
-
-  if (!authorization) {
-    return new Response("No authorization header", { status: 400 });
-  }
-
-  const event = receiver.receive(body, authorization);
-
-  if (event.event === "ingress_started") {
-    await db.stream.update({
-      where: {
-        ingressId: event.ingressInfo?.ingressId,
-      },
-      data: {
-        isLive: true,
-      },
-    });
-  }
-
-  if (event.event === "ingress_ended") {
-    await db.stream.update({
-      where: {
-        ingressId: event.ingressInfo?.ingressId,
-      },
-      data: {
-        isLive: false,
-      },
-    });
-  }
-    res.status(200).json({ success: true });
-  } catch (error) {
-    console.error(error);
-
-    // 에러 응답 반환
-    res.status(500).json({ success: false, error: 'Internal Server Error' });
   }
 }
